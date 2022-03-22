03e1f1dc84abb618fd73db0cd42d104a8a8131ce
Timur Dolgikh <89405961+DolgikhTi@users.noreply.github.com>
2022-03-22 19:38:00 +0300

    Add files via upload

---
+++ TASKS_KOTLIN_CONVENTIONS/FOR_LOOP/src/DateUtil.kt
@@ -0,0 +1,11 @@
+import java.util.*
+
+fun MyDate.followingDate(): MyDate {
+    val c = Calendar.getInstance()
+    c.set(year, month, dayOfMonth)
+    val millisecondsInADay = 24 * 60 * 60 * 1000L
+    val timeInMillis = c.timeInMillis + millisecondsInADay
+    val result = Calendar.getInstance()
+    result.timeInMillis = timeInMillis
+    return MyDate(result.get(Calendar.YEAR), result.get(Calendar.MONTH), result.get(Calendar.DATE))
+}
